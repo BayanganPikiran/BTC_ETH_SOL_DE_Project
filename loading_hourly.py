ba81d255f0b0595d6d121bfbfdb3ff28a1b7e3be
Andre LaFlamme <bayangan.pikiran@gmail.com>
2024-01-11 05:14:17 +0700

    started load_csc_to_db_hourly() by checking for the csv path, reading the csv with pandas, and inlcuding tries/exception and logging

--- loading_hourly.py
+++ loading_hourly.py
@@ -22,3 +22,17 @@ SOL_HOURLY_CSV_PATH = os.getenv('SOL_HOURLY_CSV_PATH')
 
 # Feature flag
 DRY_RUN = os.getenv('DRY_RUN', 'False').lower() == 'true'
+
+
+def load_csv_to_db_hourly(csv_file_path: str, table_name: str, db_connection: psycopg2.extensions.connection) -> None:
+    # Check if the CSV file exists
+    if not os.path.exists(csv_file_path):
+        logging.error(f"CSV file not found: {csv_file_path}")
+        return
+    # Read CSV file using Pandas
+    try:
+        data = pd.read_csv(csv_file_path)
+    except Exception as e:
+        logging.error(f"Error reading the CSV file {csv_file_path}: {e}")
+        return
+
